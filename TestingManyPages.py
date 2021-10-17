7378be42ab57aab9a8d62ca024520ddce9559627
Julian Castoro <juliancastoro@Julians-MacBook-Pro.local>
2021-10-16 21:50:10 -0400

    added dynamic page fetching

--- TestingManyPages.py
+++ TestingManyPages.py
@@ -1,7 +1,9 @@
 from bs4 import BeautifulSoup as bs
 import requests
+import math
 
 
+# better to have a function that creates the html object and pass that to these functions below
 def pull_pages(url):
 
     headers = {
@@ -18,3 +20,20 @@ def pull_pages(url):
     for review in all_reviews:
         print(review.get_text(strip=True))
         print("\n")
+
+
+def get_number_of_pages(url):
+    reviews_per_page = 10
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36',
+        "Upgrade-Insecure-Requests": "1", "DNT": "1",
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+        "Accept-Language": "en-US,en;q=0.5", "Accept-Encoding": "gzip, deflate"}
+
+    response = requests.get(url, headers=headers)
+    html = response.content
+    soup = bs(html, "lxml")
+    full_string = soup.find("h2", class_="ugc-list__reviews__heading").get_text()
+
+    number_of_reviews = int(full_string[0:2])
+    return math.ceil(number_of_reviews/reviews_per_page)
--- main.py
+++ main.py
@@ -1,16 +1,36 @@
 from requestingPage import *
 from TestingManyPages import *
-import pandas
+import numpy as np
+import pandas as pd
 
-if __name__ == '__main__' :
+# Think there is a better way to construct the URL using requests
+#   each partNumber and partId is unique to the product, this is all we will need to change
+#   Base url will be the same
+#   Thinking we store products in a dict with k: str productName v: tuple partNumber, partId
+partNumber = 91615
+partId = 119264
+rootURL = "https://www.chewy.com/app/product-reviews/rel" \
+          "oad?partNumber=" + str(partNumber) + "&=" + str(partId) + "&reviewSort=NEWEST&reviewFilter=ALL_STARS&" \
+          "pageNumber="
+
+
+if __name__ == '__main__':
     # pullPage()
-    for page in range(0, 7):
+    page = 1
+    maxPages = 1
+    while page <= int(maxPages):
+
         currPage = "https://www.chewy.com/app/product-reviews/rel" \
           "oad?partNumber=91615&id=119264&reviewSort=NEWEST&reviewFilter=ALL_STARS&pageNumber=" + str(page)
         print("Page: " + str(page))
         print("_____________________________________________")
         pull_pages(currPage)
         print("\n")
+        #maxPages = get_number_of_pages(currPage)
+
+        # iterating
+        maxPages = get_number_of_pages(currPage)
+        page += 1
 
 
 
